7e06acf501ad02ad9368cbc3f1dfffd151b2aa20
Krishan Thakur <krishan97@users.noreply.github.com>
2017-02-06 14:25:53 +0530

    Delete sampleFile.php

--- sampleFile.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!empty($_GET['devIceNumber'])){
-require 'application/autoload.php';
-$appObj=new AppModel();    // create obj of App Model Class
-$getData=$appObj->app_install($_POST['devIceNumber']);  // Call the method
-
-}
-?>
